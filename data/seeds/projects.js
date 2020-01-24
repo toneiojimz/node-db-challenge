18d5827cdf77624918a1f413af4b7c0f538a77eb
toneiojimz <ajimeneza77@gmail.com>
2020-01-24 12:15:51 -0800

    adding and retrieving projects works

--- data/seeds/projects.js
+++ data/seeds/projects.js
@@ -3,7 +3,7 @@ exports.seed = function(knex){
     return knex('projects').truncate()
         .then(function() {
             return knex('projects').insert([
-                {id: 1, description: "create database", name: "appie", steps: "gather information, install dependencies, test app"}
+                
             ]);
         });
 };
